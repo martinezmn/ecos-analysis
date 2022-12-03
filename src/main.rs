240596101ebdcb5e521ea7286c6d77db77068050
Gabriel Martinez <gabrielmn378@gmail.com>
2022-12-03 23:39:53 +0000

    serdi bugs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,37 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate serde;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+use serde::json::{Json, Value, json};
+use serde::{Serialize, Deserialize};
+
+mod teste;
+
+// #[get("/<id>/<name>")]
+// fn project() -> String {
+//     return teste::print_hello()
+// }
+
+// #[get("/projects")]
+// fn index() -> content::Json<&'static str> {
+//     content::Json("{ hi: \"world\" }")
+// }
+
+#[post("/", format = "json", data = "<message>")]
+async fn new(message: Json<Message<'_>>, list: Messages<'_>) -> Value {
+    let mut list = list.lock().await;
+    let id = list.len();
+    list.push(message.message.to_string());
+    json!({ "status": "ok", "id": id })
+}
+
+#[catch(404)]
+fn not_found() -> Value {
+    json!({
+        "status": "error",
+        "reason": "Resource was not found."
+    })
 }
 
 fn main() {
---
+++ src/teste.rs
@@ -0,0 +1,7 @@
+pub fn print_hello() -> String {
+    return "Hello, worlssssd!".to_string()
+}
+
+pub fn print_hellos() -> String {
+    return "Hello, dssd!".to_string()
+}
